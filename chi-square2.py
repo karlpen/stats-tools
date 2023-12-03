ac7967b72a816b150171062c72075aa8c390f5a4
karlpen <31706903+karlpen@users.noreply.github.com>
2023-12-03 11:47:01 -0300

    Most recent version - 03-DEC-2023
    - added a graphic interface (tkinter)
    - if the use gives wrong input for number of point in the series, print error message (need to to the same for numerators/denominators)
    - quit button 
    
    for the future:
    - graph with data points
    - check num-denom as above
    - import xls files

--- chi-square2.py
+++ chi-square2.py
@@ -33,8 +33,12 @@ def run_statistical_test():
     output_area.insert(tk.END, f"Chi-square statistic: {resultado_chi2}\nP-value: {valor_p_final}")  # Insert new output
     output_area.config(state=tk.DISABLED)  # Disable editability
 
+def exit_window():
+    window.destroy()
+
 window = tk.Tk()
-window.title("Teste qui-quadrado de tendência")
+window.title("Teste qui-quadrado de tendência - SCIENTIA TOOLS")
+window.geometry('500x700')
 
 input_label_num_points = tk.Label(window, text="Número de pontos na série:")
 input_label_num_points.pack()
@@ -45,19 +49,27 @@ input_numerador_fields = []
 input_denominador_fields = []
 
 def create_input_fields():
-    num_points = int(input_field_num_points.get())
-    for i in range(num_points):
-        lbl_numerador = tk.Label(window, text=f"Numerador for point {i+1}:")
-        lbl_numerador.pack()
-        entry_numerador = tk.Entry(window)
-        entry_numerador.pack()
-        input_numerador_fields.append(entry_numerador)
+    try:
+        num_points = int(input_field_num_points.get())
+    except ValueError:
+        output_area.config(state=tk.NORMAL)  # Enable editability
+        output_area.delete("1.0", tk.END)    # Clear previous output
+        output_area.insert(tk.END, f"Valor inválido")  # Insert new output
+        output_area.config(state=tk.DISABLED)  # Disable editability  
+   
+    else:
+        for i in range(num_points):
+            lbl_numerador = tk.Label(window, text=f"Numerador for point {i+1}:")
+            lbl_numerador.pack()
+            entry_numerador = tk.Entry(window)
+            entry_numerador.pack()
+            input_numerador_fields.append(entry_numerador)
 
-        lbl_denominador = tk.Label(window, text=f"Denominador for point {i+1}:")
-        lbl_denominador.pack()
-        entry_denominador = tk.Entry(window)
-        entry_denominador.pack()
-        input_denominador_fields.append(entry_denominador)
+            lbl_denominador = tk.Label(window, text=f"Denominador for point {i+1}:")
+            lbl_denominador.pack()
+            entry_denominador = tk.Entry(window)
+            entry_denominador.pack()
+            input_denominador_fields.append(entry_denominador)
 
 create_input_fields_button = tk.Button(window, text="Entrar com os valores da série", command=create_input_fields)
 create_input_fields_button.pack()
@@ -69,5 +81,8 @@ output_label = tk.Label(window, text="Saída:")
 output_label.pack()
 output_area = tk.Text(window, height=10, width=40, state=tk.DISABLED)
 output_area.pack()
+
+exit_button = tk.Button(window, text="Sair", command=exit_window)
+exit_button.pack()
 
 window.mainloop()
